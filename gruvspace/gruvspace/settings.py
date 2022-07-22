ff7d43b0736e12266bd40d52127537ea7db6ca99
Renda Nyamande <rendanyamande44@gmail.com>
2022-07-22 19:08:14 +0200

    safety is key

--- gruvspace/gruvspace/settings.py
+++ gruvspace/gruvspace/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 # import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ktxg@cb#4q50$55ivj!603y^_njbn8=f@*x390@wl@02yicpa^'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
